7b567d92258010d065a36a278248d54b8186beb0
Daniel <116485787+dkwagner40@users.noreply.github.com>
2025-02-26 08:20:13 +0100

    Create vampire.py

---
+++ vampire.py
@@ -0,0 +1,10 @@
+name = 'Daniel'
+age = 3000
+if name == 'Daniel':
+    print('Hi, Daniel.')
+elif age < 12:
+    print('You are not Daniel, kid.')
+elif age > 2000:
+    print('Unlike you, Daniel is not an immortal vampire.')
+elif age > 100:
+    print('You are not Daniel, pop.')
